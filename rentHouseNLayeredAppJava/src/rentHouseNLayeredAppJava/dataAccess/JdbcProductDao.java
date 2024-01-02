658dbc23687fd3d11839765a4b3347a1313d79b9
happydll <85785507+happydll@users.noreply.github.com>
2024-01-03 02:17:27 +0300

    Update JdbcProductDao.java

--- rentHouseNLayeredAppJava/src/rentHouseNLayeredAppJava/dataAccess/JdbcProductDao.java
+++ rentHouseNLayeredAppJava/src/rentHouseNLayeredAppJava/dataAccess/JdbcProductDao.java
@@ -1,17 +1,35 @@
 package rentHouseNLayeredAppJava.dataAccess;
 
+import rentHouseNLayeredAppJava.core.logging.Logging;
 import rentHouseNLayeredAppJava.entities.Product;
 
 public class JdbcProductDao implements ProductDao {
 
+	private Logging[] loggers;
+	
+	public JdbcProductDao(Logging[] loggers) {
+		super();
+		this.loggers = loggers;
+	}
+
 	public void add(Product product) {
 		//db conn string ve ekleme kodu 
-		System.out.println("Jdbc ile eklendi");
+		//System.out.println("Jdbc ile eklendi");
+		
+		for (Logging logging: loggers) {
+			String data = "Jdbc ile veritabanına eklendi: " + product.getName();
+			logging.log(data);
+		}
 	}
 	
 	public void delete(Product product) {
 		//db conn string ve silme kodu 
-		System.out.println("Jdbc ile silindi");
+		//System.out.println("Jdbc ile silindi");
+		
+		for (Logging logging: loggers) {
+			String data = "Jdbc ile veritabanına eklendi: " + product.getName();
+			logging.log(data);
+		}
 	}
 	
 }
